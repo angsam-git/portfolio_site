a0dff2abef0746b1ec9f852162f341e4348ce1d0
Angel Samuel Mendez <59542697+angsam-git@users.noreply.github.com>
2021-12-13 10:50:54 -0500

    Add files via upload

--- static/wrapper.js
+++ static/wrapper.js
@@ -7,43 +7,29 @@ var gits = document.getElementsByClassName('proj_git');
 
 
 function format_pg(){
-  var i = 0;
-  
-  while(i < containers.length)
+  var h;
+  for(var i = 0; i < containers.length; i++)
   {
     containers[i].style.flexDirection = "column";
     image_containers[i].style.width = '100%';
-    var h = images[2].clientHeight * 2;
-    if(h > 0)
-      containers[i].style.height = h.toString() + "px";
-    else
-    {
-      images = document.getElementsByClassName('proj_img');
-      continue;
-    }
-      
+    h = images[2].clientHeight * 2;
+    containers[i].style.height = h.toString() + "px";
     texts[i].style.height = images[2].clientHeight;
     texts[i].style.width = '100%';
     texts[i].style.fontSize = '4vw';
     gits[i].style.left = '60px';
     gits[i].style.height = '5%';
-    i++;
   }
 }
 
 function format_pg_b(){
+  var h;
   for(var i = 0; i < containers.length; i++)
     {
       containers[i].style.flexDirection = "row";
       image_containers[i].style.width = '50%';
-      var h = images[2].clientHeight;
-      if(h > 0)
-        containers[i].style.height = h.toString() + "px";
-      else
-      {
-        images = document.getElementsByClassName('proj_img');
-        continue;
-      }
+      h = images[2].clientHeight;
+      containers[i].style.height = h.toString() + "px";
       texts[i].style.height = h;
       texts[i].style.width = '50%';
       texts[i].style.fontSize = '1.9vw';
@@ -52,19 +38,9 @@ function format_pg_b(){
     }
 }
 
-if (window.innerWidth < screen.width / 1.9)
-{
-  format_pg();
-}
-
-else
-{
-  format_pg_b();
-}
-
 function wrapDiv(){
   
-  if (window.innerWidth < screen.width / 1.9)
+  if ((window.innerWidth < screen.width / 1.9) || /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) )
   {
     format_pg();
   }
